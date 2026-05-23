03560aea4c80798f7f2c93dcec637dc5de9b1271
Nandu2k03 <lingamnandeswar@gmail.com>
2026-05-23 12:38:39 +0530

    First

---
+++ nandu.py
@@ -0,0 +1,17 @@
+# This is a basic Python program
+
+# Print a message
+print("Hello, world!")
+
+# Take input from the user
+name = input("Enter your name: ")
+
+# Print a personalized message
+print("Hello,", name)
+
+# Simple calculation
+a = 5
+b = 3
+sum_result = a + b
+
+print("The sum of", a, "and", b, "is", sum_result)
